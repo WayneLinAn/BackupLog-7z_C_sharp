83b231d691b39b1ce3bb6c1b91d4be3aaab5e432
WayneLinAn <waynelinhuaian@gmail.com>
2022-02-10 08:37:00 +0800

    3

--- Backup_form/Form1.Designer.cs
+++ Backup_form/Form1.Designer.cs
@@ -32,13 +32,9 @@ namespace Backup_form
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // timer1
@@ -47,7 +43,7 @@ namespace Backup_form
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(471, 93);
+            this.button1.Location = new System.Drawing.Point(301, 70);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(114, 23);
             this.button1.TabIndex = 0;
@@ -55,41 +51,11 @@ namespace Backup_form
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(200, 319);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "壓縮";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(459, 318);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "解壓縮";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(459, 166);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "建立資料夾";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.Location = new System.Drawing.Point(211, 45);
+            this.label1.Location = new System.Drawing.Point(41, 22);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(374, 21);
             this.label1.TabIndex = 4;
@@ -97,7 +63,7 @@ namespace Backup_form
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(405, 93);
+            this.textBox1.Location = new System.Drawing.Point(235, 70);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(60, 22);
             this.textBox1.TabIndex = 5;
@@ -108,35 +74,21 @@ namespace Backup_form
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label2.Location = new System.Drawing.Point(227, 93);
+            this.label2.Location = new System.Drawing.Point(57, 70);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(172, 16);
             this.label2.TabIndex = 6;
             this.label2.Text = "想要處理幾天前的資料:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(611, 253);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 7;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button5);
+            this.ClientSize = new System.Drawing.Size(457, 118);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -150,13 +102,9 @@ namespace Backup_form
 
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button5;
     }
 }
 
--- Backup_form/bin/Debug/ai_mgr/reset.bat
+++
@@ -1,4 +0,0 @@
-copy  %~dp0\bak\*.log %~dp0
-
-
-rmdir /s/q backuplog 
